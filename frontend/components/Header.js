3ee4826d3f84f19d3268c4ef8a244ae3cb3547db
Abhiraj Thakur <abhirajthakur124@gmail.com>
2023-02-26 11:35:41 +0530

    change link to view code in Header.js

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="flex flex-row justify-between items-center border-b-2 p-4">
       <Link
-        href="https://goerli.etherscan.io/address/0x43158ACC8E43b851ABA84881275De3050611030c#code"
+        href="https://goerli.etherscan.io/address/0x31849D0F5Ac736cA4386245A42eD54F694dBc379#code"
         target="_blank"
       >
         <h1 className="p-4 font-normal text-3xl">Random IPFS NFT</h1>
